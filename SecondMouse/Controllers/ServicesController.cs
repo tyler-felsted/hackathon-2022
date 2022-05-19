55f35fef198ae2a7f26d4e8ed0833317c29777b5
RAMQUEST\cschall <cschall@ramquest.com>
2022-05-19 10:34:59 -0500

    cleaned up controller endpoints

--- SecondMouse/Controllers/ServicesController.cs
+++ SecondMouse/Controllers/ServicesController.cs
@@ -20,32 +20,17 @@ namespace SecondMouse.Controllers
             _context = context;
         }
 
-        //api/Services
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Services>>> GetServices()
-        {
-            return await _context.Services.ToListAsync();
-        }
-
-        [HttpGet("{state}/{county}")]
-        public async Task<ActionResult<Services>> GetFileServices(string state, string county)
-        {
-            var service = await _context.Services.SingleOrDefaultAsync(s => s.state == state && s.county == county);
-
-            if (service == null)
-            {
-                return NotFound();
-            }
-
-            return service;
-        }
-
         [Route("FileServiceCheck")]
         [HttpPost]
         public async Task<ActionResult<FileServiceCheck>> PostFileService(File file)
         {
             var service = await _context.Services.SingleOrDefaultAsync(s => s.state == file.state && s.county == file.county);
             var signingService = await _context.SigningServices.Where(ss => ss.state == file.state).ToListAsync();
+
+            if (service == null)
+            {
+                return NotFound($"No services have been found for {file.county}, {file.state}");
+            }
 
             FileServiceCheck fileServiceCheckResults = new FileServiceCheck();
             fileServiceCheckResults.file = file;
